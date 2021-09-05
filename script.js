7b4b43b58fbb8f201d55d0520aa89c364e94947d
Hamza Anwar <hamzaanwardev@gmail.com>
2021-09-05 23:28:17 +0500

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ textarea.addEventListener('keyup', (e) => {
 } )
 
 function createTags(input) {
-    const tags = input.split('');
-    console.log(tags);
+    const tags = input.split('').filter(tag => tag.trim()
+    !== '' ).map(tag => tag.trim())
+
+    tagsEl.innerHTML= '';
+    
 }
